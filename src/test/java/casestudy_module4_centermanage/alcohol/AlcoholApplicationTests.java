ec0169a3ffd43b8a018ee632c92113c89b5af85d
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-22 10:34:00 +0700

    case study module 4: center manage

---
+++ src/test/java/casestudy_module4_centermanage/alcohol/AlcoholApplicationTests.java
@@ -0,0 +1,13 @@
+package casestudy_module4_centermanage.alcohol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
