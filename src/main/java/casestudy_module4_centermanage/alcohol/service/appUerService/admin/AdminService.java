cd9dd24a2000ddaa0cf29c48bc8f6eec387ac545
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-23 15:14:43 +0700

    tạo AppUserService

--- src/main/java/casestudy_module4_centermanage/alcohol/service/appUerService/admin/AdminService.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/service/appUerService/admin/AdminService.java
@@ -1,8 +1,25 @@
 package casestudy_module4_centermanage.alcohol.service.appUerService.admin;
 
+import casestudy_module4_centermanage.alcohol.model.Student;
+import casestudy_module4_centermanage.alcohol.model.Teacher;
+import casestudy_module4_centermanage.alcohol.model.Warden;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AdminService {
+public class AdminService implements IAdminService{
 
+    @Override
+    public Teacher insertTeacher() {
+        return null;
+    }
+
+    @Override
+    public Student insertStudent() {
+        return null;
+    }
+
+    @Override
+    public Warden insertWarden() {
+        return null;
+    }
 }
