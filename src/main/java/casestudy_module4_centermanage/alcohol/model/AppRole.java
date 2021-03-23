8c71b170bc94084c7dc1338f7b51d4b3a83d2a84
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-23 08:44:28 +0700

    creat class AppRole

--- src/main/java/casestudy_module4_centermanage/alcohol/model/AppRole.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/model/AppRole.java
@@ -1,23 +1,16 @@
 package casestudy_module4_centermanage.alcohol.model;
 
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class AppRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String password;
-    private String phone;
-    private String address;
-    private String avatar;
-    @Transient
-    private MultipartFile avatarMul;
-    @ManyToOne
-    private AppRole role;
+    private String name;
 
     public AppRole() {
     }
@@ -30,59 +23,11 @@ public class AppRole {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public MultipartFile getAvatarMul() {
-        return avatarMul;
-    }
-
-    public void setAvatarMul(MultipartFile avatarMul) {
-        this.avatarMul = avatarMul;
-    }
-
-    public AppRole getRole() {
-        return role;
-    }
-
-    public void setRole(AppRole role) {
-        this.role = role;
+    public void setName(String name) {
+        this.name = name;
     }
 }
