b81f934d60f51fa9e4357635c2c31e6cb1230c0d
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-23 11:25:45 +0700

    create class Diary

--- src/main/java/casestudy_module4_centermanage/alcohol/model/Diary.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/model/Diary.java
@@ -1,13 +1,60 @@
 package casestudy_module4_centermanage.alcohol.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String content;
+    private Date date;
+    @ManyToOne
+    private Classes classes;
+    @ManyToOne
+    private Student student;
+
+    public Diary() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Classes getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Classes classes) {
+        this.classes = classes;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
