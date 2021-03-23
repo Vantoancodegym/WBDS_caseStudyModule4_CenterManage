c5a22acd820ee7a032e769d74012f43f2fdb1e7a
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-23 08:40:45 +0700

    creat class AppUser

---
+++ src/main/java/casestudy_module4_centermanage/alcohol/model/AppRole.java
@@ -0,0 +1,88 @@
+package casestudy_module4_centermanage.alcohol.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.*;
+
+@Entity
+public class AppRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String phone;
+    private String address;
+    private String avatar;
+    @Transient
+    private MultipartFile avatarMul;
+    @ManyToOne
+    private AppRole role;
+
+    public AppRole() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public MultipartFile getAvatarMul() {
+        return avatarMul;
+    }
+
+    public void setAvatarMul(MultipartFile avatarMul) {
+        this.avatarMul = avatarMul;
+    }
+
+    public AppRole getRole() {
+        return role;
+    }
+
+    public void setRole(AppRole role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/casestudy_module4_centermanage/alcohol/model/AppUser.java
@@ -0,0 +1,4 @@
+package casestudy_module4_centermanage.alcohol.model;
+
+public class AppUser {
+}
