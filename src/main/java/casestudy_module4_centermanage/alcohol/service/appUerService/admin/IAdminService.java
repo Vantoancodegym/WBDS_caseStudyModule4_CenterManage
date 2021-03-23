79e8d0ce7f0ffe4c4f1d3f7f62c360cc087937bc
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-23 15:35:33 +0700

    thêm phương thức insertClasses

--- src/main/java/casestudy_module4_centermanage/alcohol/service/appUerService/admin/IAdminService.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/service/appUerService/admin/IAdminService.java
@@ -1,5 +1,6 @@
 package casestudy_module4_centermanage.alcohol.service.appUerService.admin;
 
+import casestudy_module4_centermanage.alcohol.model.Classes;
 import casestudy_module4_centermanage.alcohol.model.Student;
 import casestudy_module4_centermanage.alcohol.model.Teacher;
 import casestudy_module4_centermanage.alcohol.model.Warden;
@@ -8,5 +9,5 @@ public interface IAdminService {
     Teacher insertTeacher(Teacher teacher);
     Student insertStudent(Student student);
     Warden insertWarden(Warden warden);
-
+    Classes insertClasses(Classes classes);
 }
