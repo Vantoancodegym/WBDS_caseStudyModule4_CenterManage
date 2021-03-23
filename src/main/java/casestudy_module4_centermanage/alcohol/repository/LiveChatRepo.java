52523782176c0576591961a71dda4291aefbad6b
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-23 15:03:10 +0700

    update livechat repo

--- src/main/java/casestudy_module4_centermanage/alcohol/repository/LiveChatRepo.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/repository/LiveChatRepo.java
@@ -1,8 +1,9 @@
 package casestudy_module4_centermanage.alcohol.repository;
 
+import casestudy_module4_centermanage.alcohol.model.LiveChat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LiveChatRepo extends JpaRepository<LiveChatRepo,Long> {
+public interface LiveChatRepo extends JpaRepository<LiveChat,Long> {
 }
