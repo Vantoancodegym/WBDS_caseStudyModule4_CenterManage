d885155ddb7958b60a640ab5f892efaf7317bf5a
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-23 09:10:10 +0700

    create class AppSecurityCongiguration

--- src/main/java/casestudy_module4_centermanage/alcohol/config/AppSecurityConfiguration.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/config/AppSecurityConfiguration.java
@@ -1,5 +1,48 @@
 package casestudy_module4_centermanage.alcohol.config;
 
-public class AppSecurityConfiguration {
+import casestudy_module4_centermanage.alcohol.service.appUerService.IAppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@EnableWebSecurity
+public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private IAppUserService appUserService;
+    @Autowired
+    private LoginSuccessHandle loginSuccessHandle;
+    @Autowired
+    private AccessDinyHandle accesDinedHandler;
+    //    xac thuc bo nho
+    //    @Bean
+//    public UserDetailsService userDetailsService(){
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        InMemoryUserDetailsManager memoryUserDetailsManager = new InMemoryUserDetailsManager();
+//        memoryUserDetailsManager.createUser(userBuilder.username("hung").password("123456").roles("USER").build());
+//        memoryUserDetailsManager.createUser(userBuilder.username("vohung").password("123456").roles("ADMIN").build());
+//        return memoryUserDetailsManager;
+//    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService((UserDetailsService) appUserService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll().and()
+                .formLogin().loginPage("/login").failureForwardUrl("/failLogin").successHandler(loginSuccessHandle)
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .and().exceptionHandling().accessDeniedHandler(accesDinedHandler);
+        http.csrf().disable();
+
+    }
 }
