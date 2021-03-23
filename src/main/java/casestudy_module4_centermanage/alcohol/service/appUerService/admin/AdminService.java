641e8ab318e53c40786f575168de5067c88b06f5
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-23 15:16:32 +0700

    tiêm student repo

--- src/main/java/casestudy_module4_centermanage/alcohol/service/appUerService/admin/AdminService.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/service/appUerService/admin/AdminService.java
@@ -3,6 +3,7 @@ package casestudy_module4_centermanage.alcohol.service.appUerService.admin;
 import casestudy_module4_centermanage.alcohol.model.Student;
 import casestudy_module4_centermanage.alcohol.model.Teacher;
 import casestudy_module4_centermanage.alcohol.model.Warden;
+import casestudy_module4_centermanage.alcohol.repository.StudentRepo;
 import casestudy_module4_centermanage.alcohol.repository.TeacherRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 public class AdminService implements IAdminService{
     @Autowired
     private TeacherRepo teacherRepo;
+    @Autowired
+    private StudentRepo studentRepo;
     @Override
     public Teacher insertTeacher() {
         return null;
