a624f0752ac42aedb6a67e4b4ca878ee6363684b
Ngô Minh Hiếu <hieunm92@gmail.com>
2021-03-24 11:26:08 +0700

    creat query

---
+++ src/main/java/casestudy_module4_centermanage/alcohol/controller/WardenController.java
@@ -0,0 +1,22 @@
+package casestudy_module4_centermanage.alcohol.controller;
+
+
+import casestudy_module4_centermanage.alcohol.service.wardenService.IWardenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("wardens")
+public class WardenController {
+
+    @Autowired
+    private IWardenService wardenService;
+
+    @GetMapping()
+    public ModelAndView showAll (){
+
+    }
+}
