72d5c38a7f134d9087f0089ff9ec548f53877493
nguyenvanduyet <73922657+nguyenvanduyet@users.noreply.github.com>
2021-03-24 11:10:14 +0700

    delete confix

--- src/main/java/casestudy_module4_centermanage/alcohol/repository/StudentRepo.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/repository/StudentRepo.java
@@ -9,10 +9,8 @@ import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student,Long> {
-<<<<<<< HEAD
     @Query(value = "select count(*) from teacher_classes inner join student\n" +
             "on teacher_classes.classes_id = student.classes_id",nativeQuery = true)
     int countStudentForTeacher();
-=======
->>>>>>> 80a1518af7c4bc895bb7941655d5467a2cfbaada
+
 }
