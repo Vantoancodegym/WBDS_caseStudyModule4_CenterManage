33e2d73c6f79cc7482b53a579c8a752bf0845b62
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-24 12:04:21 +0700

    finish method getTop5StudentHaveBigScore

--- src/main/java/casestudy_module4_centermanage/alcohol/repository/StudentRepo.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/repository/StudentRepo.java
@@ -12,4 +12,8 @@ public interface StudentRepo extends JpaRepository<Student,Long> {
     @Query(value = "select count(*) from teacher_classes inner join student\n" +
             "on teacher_classes.classes_id = student.classes_id",nativeQuery = true)
     int countStudentForTeacher();
+    // sua thuoc tinh student them avgScore
+    @Query(value = "select app_user.display_name,avgScore from (select app_user_id ,classes_id,student_id,(score_theory+score_lab+ccore_attitude)/3 as avgScore\n" +
+            "from score join student s on score.student_id = s.id order by avgScore desc limit 5) as abd join app_user on app_user_id = app_user.id",nativeQuery = true)
+    List<Student> getTop5StudentHaveBigScore();
 }
