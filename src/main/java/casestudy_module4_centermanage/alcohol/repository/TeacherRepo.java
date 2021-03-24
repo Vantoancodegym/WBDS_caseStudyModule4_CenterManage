f601510671e9d6ea94ddb805531535680d1929c4
nguyenvanduyet <73922657+nguyenvanduyet@users.noreply.github.com>
2021-03-24 09:17:42 +0700

    add service teacher

--- src/main/java/casestudy_module4_centermanage/alcohol/repository/TeacherRepo.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/repository/TeacherRepo.java
@@ -3,6 +3,7 @@ package casestudy_module4_centermanage.alcohol.repository;
 import casestudy_module4_centermanage.alcohol.model.Classes;
 import casestudy_module4_centermanage.alcohol.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
