607e86f6dbeefc96d2793366b8f6cb4d567b4244
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-24 10:45:14 +0700

    add method countteacher

--- src/main/java/casestudy_module4_centermanage/alcohol/repository/TeacherRepo.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/repository/TeacherRepo.java
@@ -2,8 +2,11 @@ package casestudy_module4_centermanage.alcohol.repository;
 
 import casestudy_module4_centermanage.alcohol.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherRepo extends JpaRepository<Teacher,Long> {
+    @Query(value = "select count(*) from teacher",nativeQuery = true)
+    int countTeacher();
 }
