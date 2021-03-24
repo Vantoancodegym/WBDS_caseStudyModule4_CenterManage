34d5ab9ba4c14afbd370202c01249d52903cfd39
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-24 14:55:11 +0700

    test JSON method insertClasses ok

--- src/main/java/casestudy_module4_centermanage/alcohol/controller/AdminController.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/controller/AdminController.java
@@ -33,8 +33,8 @@ public class AdminController {
         adminService.insertAppUser(warden.getAppUser());
         return new ResponseEntity(adminService.insertWarden(warden),HttpStatus.OK);
     }
-//    @PostMapping("insertClasses")
-//    public ResponseEntity<Classes> insertClasses(@RequestBody Classes classes){
-//        return new ResponseEntity<>(adminService.insertClasses(classes),HttpStatus.OK);
-//    }
+    @PostMapping("insertClasses")
+    public ResponseEntity<Classes> insertClasses(@RequestBody Classes classes){
+        return new ResponseEntity<>(adminService.insertClasses(classes),HttpStatus.OK);
+    }
 }
