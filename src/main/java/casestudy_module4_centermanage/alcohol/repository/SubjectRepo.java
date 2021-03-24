f6c79ade7d12482c084fb61f7ea04cf11448e1df
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-24 10:33:22 +0700

    add query for method countSubject

--- src/main/java/casestudy_module4_centermanage/alcohol/repository/SubjectRepo.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/repository/SubjectRepo.java
@@ -2,8 +2,11 @@ package casestudy_module4_centermanage.alcohol.repository;
 
 import casestudy_module4_centermanage.alcohol.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubjectRepo extends JpaRepository<Subject,Long> {
+    @Query(value = "select count(*) from subject",nativeQuery = true)
+    int countSubject();
 }
