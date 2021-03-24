9adeb3ed873e8f078e00a87cc440fe4b8fe031c0
MungC2010G2 <zhaiduong26z@gmail.com>
2021-03-24 21:14:46 +0700

    add method editStatusForStudent

--- src/main/java/casestudy_module4_centermanage/alcohol/service/wardenService/IWardenService.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/service/wardenService/IWardenService.java
@@ -1,11 +1,8 @@
 package casestudy_module4_centermanage.alcohol.service.wardenService;
 
-import casestudy_module4_centermanage.alcohol.model.Classes;
+
 import casestudy_module4_centermanage.alcohol.model.Student;
-import casestudy_module4_centermanage.alcohol.model.Warden;
-import casestudy_module4_centermanage.alcohol.service.IService;
-import org.aspectj.weaver.patterns.IScope;
 
-public interface IWardenService extends IService<Warden> {
-
+public interface IWardenService {
+    Student editStatusForStudent(Student student);
 }
