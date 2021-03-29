3afaff3f1abda02f26410626172a049e44525d7c
nguyenvanduyet <73922657+nguyenvanduyet@users.noreply.github.com>
2021-03-29 08:33:04 +0700

    add teacher class

--- src/main/java/casestudy_module4_centermanage/alcohol/model/Teacher_classes.java
+++ src/main/java/casestudy_module4_centermanage/alcohol/model/Teacher_classes.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 public class Teacher_classes {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne
     private Teacher teacher;
